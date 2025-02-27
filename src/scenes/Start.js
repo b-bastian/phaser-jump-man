493ee7fdbf28df36b5c23d6054448812bad1dd92
b-bastian <bbrugger@schueler.hakzell.at>
2025-02-27 21:50:59 +0100

    add

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -1,6 +1,7 @@
 export class Start extends Phaser.Scene {
   platforms;
   player;
+  stars;
 
   constructor() {
     super("Start");
@@ -28,9 +29,31 @@ export class Start extends Phaser.Scene {
     this.platforms.create(50, 250, "ground");
     this.platforms.create(750, 220, "ground");
 
+    // player
     this.player = this.physics.add.sprite(100, 450, "dude");
-    this.player.setBounce(1);
+    this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, this.platforms);
+
+    // animation
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.stars = this.physics.add.group({
+      key: "star",
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    this.stars.children.iterate((child) => {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(this.stars, this.platforms);
   }
 
   update() {}
